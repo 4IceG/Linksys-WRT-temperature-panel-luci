35de2940469d708a7b5707367b924e499c98e8b0
Rafał Wabik <4rafal@gmail.com>
2021-09-25 03:00:28 -0700

    Add files via upload

---
+++ WRT3200ACM/luci-app-temp-WRT3200ACM/luasrc/controller/temperaturecpu.lua
@@ -0,0 +1,38 @@
+-- Copyright 2019 Cezary Jackiewicz <cezary@eko.one.pl>
+-- Licensed to the public under the Apache License 2.0.
+-- Mod by IceG for Linksys WRT --
+
+module("luci.controller.temperaturecpu", package.seeall)
+
+function index()
+	if not nixio.fs.access("/sys/class/hwmon/hwmon1/temp1_input") then
+		return
+	end
+
+	entry({"admin", "status", "realtime", "temperature1"},
+		call("temperature1")).leaf = true
+end
+
+function temperature2(rv)
+	local c = nixio.fs.access("/sys/class/hwmon/hwmon1/temp1_input") and
+		io.popen("cat /sys/class/hwmon/hwmon1/temp1_input")
+
+	if c then
+		for l in c:lines() do
+			local i = l:match("^%d+")
+			if i then
+				rv[#rv + 1] = {
+					cpu = i
+				}
+			end
+		end
+		c:close()
+	end
+end
+
+function temperature1()
+	local data = { }
+	temperature2(data)
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(data)
+end
