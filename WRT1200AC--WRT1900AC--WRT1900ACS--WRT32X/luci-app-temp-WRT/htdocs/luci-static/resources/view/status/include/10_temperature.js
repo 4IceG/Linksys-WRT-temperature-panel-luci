63b62a79404f6ded59f2fa02096fdd82d717c200
Rafał Wabik <4rafal@gmail.com>
2021-09-25 02:58:11 -0700

    Add files via upload

---
+++ WRT1200AC--WRT1900AC--WRT1900ACS--WRT32X/luci-app-temp-WRT/htdocs/luci-static/resources/view/status/include/10_temperature.js
@@ -0,0 +1,2 @@
+'use strict';'require baseclass';'require fs';'require rpc';return baseclass.extend({title:_('Temperature'),render:function(data){var fields=[_('CPU'),tempcpu,_('DDR'),tempddr,_('Wi-Fi'),tempwifi,];var table=E('div',{'class':'table'});for(var i=0;i<fields.length;i+=2){table.appendChild(E('div',{'class':'tr'},[E('div',{'class':'td left','width':'33%'},[fields[i]]),E('div',{'class':'td left'},[(fields[i+1]!=null)?fields[i+1]:'?'])]));}
+return table;}});
